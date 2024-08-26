5474d780fa2a995b7a8fc0e464a1062a2731640d
gustavocadev <gustavocadev@gmail.com>
2024-08-26 14:50:56 -0500

    feat: Add multiply, subtract, and corresponding tests to utils.ts

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,14 @@
-import { add } from './utils.ts';
+import { add, multiply, subtract } from './utils.ts';
 import { test, expect } from 'bun:test';
 
 test('add', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('subtract', () => {
+  expect(subtract(2, 1)).toBe(1);
+});
+
+test('multiply', () => {
+  expect(multiply(2, 3)).toBe(6);
 });
